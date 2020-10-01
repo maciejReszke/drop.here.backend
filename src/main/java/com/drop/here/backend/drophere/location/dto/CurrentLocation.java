2e9b11d2ee16872d79792a390d09700e5b26c338
Maciej Reszke <m.reszke@paymenttechnology.pl>
2020-10-01 19:51:38 +0200

    added publishing location via websocket

---
+++ src/main/java/com/drop/here/backend/drophere/location/dto/CurrentLocation.java
@@ -0,0 +1,9 @@
+package com.drop.here.backend.drophere.location.dto;
+
+import lombok.Value;
+
+@Value
+public class CurrentLocation {
+    Double xPos;
+    Double yPos;
+}
---
+++ src/main/java/com/drop/here/backend/drophere/security/configuration/websocket/WebSocketAuthorizationSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.drop.here.backend.drophere.security.configuration.websocket;
+
+import com.drop.here.backend.drophere.authentication.account.service.PrivilegeService;
+import com.drop.here.backend.drophere.location.endpoint.LocationWebSocketController;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.security.messaging.util.matcher.SimpDestinationMessageMatcher;
+
+@Configuration
+public class WebSocketAuthorizationSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(final MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .matchers(new SimpDestinationMessageMatcher(WebSocketConfig.WEB_SOCKET_DESTINATION_PREFIX + LocationWebSocketController.ENDPOINT)).hasAnyAuthority(PrivilegeService.COMPANY_BASIC_MANAGEMENT_PRIVILEGE, PrivilegeService.COMPANY_FULL_MANAGEMENT_PRIVILEGE)
+                .matchers(new SimpDestinationMessageMatcher(LocationWebSocketController.ENDPOINT + "/**")).hasAnyAuthority(PrivilegeService.COMPANY_BASIC_MANAGEMENT_PRIVILEGE, PrivilegeService.COMPANY_FULL_MANAGEMENT_PRIVILEGE)
+                .matchers(new SimpDestinationMessageMatcher("/**")).denyAll();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
